8cb651d7f7e8339aed49c34d4d94db19b9d2c618
harpreetsahota204 <harpreetsahota204@gmail.com>
2025-01-27 15:17:25 -0600

    iterate over lis of urs

--- download_dataset.sh
+++ download_dataset.sh
@@ -12,21 +12,27 @@ weather=('ClearNoon')
 layout=('Town01' 'Town02' 'Town05' 'Town07')
 
 #Change here for any specific annotation, for example - https://huggingface.co/datasets/hoffman-lab/SkyScenes/resolve/main/Segment
-base_url=('https://huggingface.co/datasets/hoffman-lab/SkyScenes/resolve/main/Segment')
+base_url=(
+  'https://huggingface.co/datasets/hoffman-lab/SkyScenes/resolve/main/Images'
+  https://huggingface.co/datasets/hoffman-lab/SkyScenes/resolve/main/Depth
+  'https://huggingface.co/datasets/hoffman-lab/SkyScenes/resolve/main/Segment'
+  )
 
 #Change here for base download folder
 base_download_folder='SkyScenes'
 
 
-for hp in "${HP[@]}"; do
-  for w in "${weather[@]}"; do
-      for t in "${layout[@]}"; do
-        folder=$(echo "$base_url" | awk -F '/' '{print $(NF)}')
-        download_url="${base_url}/${hp}/${w}/${t}/${t}.tar.gz"
-        download_folder="${base_download_folder}/${folder}/${hp}/${w}/${t}"
-        mkdir -p "$download_folder"
-        echo "Downloading: $download_url"
-        wget -P "$download_folder" "$download_url"
-      done
+for url in "${base_url[@]}"; do
+  for hp in "${HP[@]}"; do
+    for w in "${weather[@]}"; do
+        for t in "${layout[@]}"; do
+          folder=$(echo "$url" | awk -F '/' '{print $(NF)}')
+          download_url="${url}/${hp}/${w}/${t}/${t}.tar.gz"
+          download_folder="${base_download_folder}/${folder}/${hp}/${w}/${t}"
+          mkdir -p "$download_folder"
+          echo "Downloading: $download_url"
+          wget -P "$download_folder" "$download_url"
+        done
+    done
   done
 done
